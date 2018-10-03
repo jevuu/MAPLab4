6dedf8fb01512203d922802ff4291bad92b01006
jevuu <35980711+jevuu@users.noreply.github.com>
2018-10-03 15:41:23 -0400

    Completed

--- app/src/main/java/com/example/d8/maplab4/MainActivity.java
+++ app/src/main/java/com/example/d8/maplab4/MainActivity.java
@@ -3,23 +3,34 @@ package com.example.d8.maplab4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView leftList;
-    ListView rightList;
+    GridView rightList;
 
     String[] stringArray = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Y","X","Z"};
+    String[] numberArray = new String[99];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView leftList = findViewById(R.id.leftList);
-        ArrayAdapter<String> arrayAdapterLeftList =
+        leftList = findViewById(R.id.leftList);
+        rightList = findViewById(R.id.rightList);
+
+        for(int i = 0; i < 99; i++){
+             numberArray[i] = String.valueOf(7 * (i + 1));
+        }
+        ArrayAdapter<String> leftList_Adapter =
                 new ArrayAdapter<>(this, R.layout.textview_left_list, stringArray);
-        leftList.setAdapter(arrayAdapterLeftList);
+        leftList.setAdapter(leftList_Adapter);
+
+        ArrayAdapter<String> rightList_Adapter =
+                new ArrayAdapter<>(this, R.layout.textview_right_list, numberArray);
+        rightList.setAdapter(rightList_Adapter);
     }
 }
